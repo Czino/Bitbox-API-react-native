95ee3a9ffa74b8ef1ff6f75d839bfcc30ea3466c
Eric Lehmann <eric@machinas.com>
2022-03-23 16:15:00 +0100

    Base install

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, SafeAreaView } from 'react-native'
+
+const App: React.FC = () => {
+  return <SafeAreaView>
+      <Text>Hello Bitbox!</Text>
+    </SafeAreaView>
+}
+
+export default App
