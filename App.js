9b120052aeff2410894c3a881b20602b134714a5
Eric Lehmann <eric@machinas.com>
2022-03-23 17:32:08 +0100

    Include bitbox02-api module to demonstrate error

---
+++ App.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useState } from 'react'
+import { Text, SafeAreaView } from 'react-native'
+import { useEffect } from 'react'
+const { BitBox02API, getDevicePath } = require('bitbox02-api')
+
+const App = () => {
+  const [bitBoxLoaded, setBitBoxLoaded] = useState(false)
+  const [error, setError] = useState('')
+  useEffect(() => {
+    (async () => {
+      try {
+        const devicePath = await getDevicePath()
+        const BitBox02 = new BitBox02API(devicePath)
+        setBitBoxLoaded(true)
+      } catch (e) {
+        console.error(e)
+        setError(e.message)
+      }
+    })()
+  }, [])
+
+  ErrorUtils.setGlobalHandler(err => {
+    console.error(err)
+    setError(err.message)
+  })
+
+  return <SafeAreaView style={{padding: 16}}>
+      <Text>Hello Bitbox!</Text>
+      {bitBoxLoaded
+        ? <Text>Bitbox API has been initialised</Text>
+        : <Text>Initializing Bitbox API...</Text>
+      }
+      {error
+        ? <Text style={{color: 'red', marginTop: 16}}>{error}</Text>
+        : null
+      }
+    </SafeAreaView>
+}
+
+export default App
